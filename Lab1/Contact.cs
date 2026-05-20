a63b2175fc85349bc9860275122c258ea8f9714c
Андрей <Fyucfyugc13467@gmail.com>
2026-05-20 09:50:27 +0300

    adding ContactTests.cs

--- Lab1/Contact.cs
+++ Lab1/Contact.cs
@@ -9,8 +9,14 @@ namespace ContactManager
 
         public Contact(string name, string phoneNumber)
         {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Имя не может быть пустым!");
+
+            if (string.IsNullOrWhiteSpace(phoneNumber))
+                throw new ArgumentException("Телефон не может быть пустым!");
+
             Name = name;
             PhoneNumber = phoneNumber;
         }
     }
-}
+}   
---
+++ Lab2.Tests/ContactTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ContactManager;
+
+namespace Lab2.Tests
+{
+    [TestClass]
+    public class ContactTests
+    {
+        [TestMethod]
+        public void Contact_Constructor_ValidData_CreatesContact()
+        {
+            // Arrange
+            string name = "Иван Петров";
+            string phone = "+7 (999) 123-45-67";
+
+            // Act
+            var contact = new Contact(name, phone);
+
+            // Assert
+            Assert.AreEqual(name, contact.Name);
+            Assert.AreEqual(phone, contact.PhoneNumber);
+        }
+
+        [TestMethod]
+        public void Contact_Name_CanBeChanged()
+        {
+            // Arrange
+            var contact = new Contact("Иван", "123");
+
+            // Act
+            contact.Name = "Петр";
+
+            // Assert
+            Assert.AreEqual("Петр", contact.Name);
+        }
+
+        [TestMethod]
+        public void Contact_Phone_CanBeChanged()
+        {
+            // Arrange
+            var contact = new Contact("Иван", "123");
+
+            // Act
+            contact.PhoneNumber = "456";
+
+            // Assert
+            Assert.AreEqual("456", contact.PhoneNumber);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_WhenNameIsNull_ThrowsException()
+        {
+            // Act
+            new Contact(null, "1234567890");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_WhenPhoneIsNull_ThrowsException()
+        {
+            // Act
+            new Contact("Иван Петров", null);
+        }
+    }
+}
