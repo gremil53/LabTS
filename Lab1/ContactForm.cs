e162304b0d0fc7320f1840134e0ae5e44f923485
Андрей <Fyucfyugc13467@gmail.com>
2026-05-28 19:09:12 +0300

    feat: improve ContactForm UI with labels - add name, phone and search labels - update form layout - improve UX

--- Lab1/ContactForm.cs
+++ Lab1/ContactForm.cs
@@ -28,21 +28,37 @@ namespace ContactManager
             this.Width = 500;
             this.Height = 400;
 
+            //Label для поле имя
+            Label nameLabel = new Label
+            {
+                Text = "Имя:",
+                Location = new System.Drawing.Point(10, 12),
+                Size = new System.Drawing.Size(40, 20)
+            };
+
             nameTextBox = new TextBox
             {
-                Location = new System.Drawing.Point(10, 10),
-                Width = 150
+                Location = new System.Drawing.Point(55, 10),
+                Width = 200
+            };
+
+            //Label для поле телефон
+            Label phoneLabel = new Label
+            {
+                Text = "Телефон:",
+                Location = new System.Drawing.Point(10, 42),
+                Size = new System.Drawing.Size(60, 20)
             };
 
             phoneNumberTextBox = new TextBox
             {
-                Location = new System.Drawing.Point(170, 10),
-                Width = 150
+                Location = new System.Drawing.Point(75, 40),
+                Width = 200
             };
 
             addContactButton = new Button
             {
-                Location = new System.Drawing.Point(10, 40),
+                Location = new System.Drawing.Point(10, 70),
                 Text = "Добавить",
                 Width = 100
             };
@@ -50,21 +66,29 @@ namespace ContactManager
 
             removeContactButton = new Button
             {
-                Location = new System.Drawing.Point(120, 40),
+                Location = new System.Drawing.Point(120, 70),
                 Text = "Удалить",
                 Width = 100
             };
             removeContactButton.Click += RemoveContactButton_Click;
 
+            // Label для поле поиск
+            Label searchLabel = new Label
+            {
+                Text = "Поиск:",
+                Location = new System.Drawing.Point(10, 112),
+                Size = new System.Drawing.Size(50, 20)
+            };
+
             searchTextBox = new TextBox
             {
-                Location = new System.Drawing.Point(10, 70),
+                Location = new System.Drawing.Point(65, 110),
                 Width = 200
             };
 
             searchButton = new Button
             {
-                Location = new System.Drawing.Point(220, 70),
+                Location = new System.Drawing.Point(275, 108),
                 Text = "Искать",
                 Width = 80
             };
@@ -72,19 +96,23 @@ namespace ContactManager
 
             contactsListBox = new ListBox
             {
-                Location = new System.Drawing.Point(10, 100),
-                Width = 450,
-                Height = 200
+                Location = new System.Drawing.Point(10, 145),
+                Width = 460,
+                Height = 220
             };
 
+            this.Controls.Add(nameLabel);
             this.Controls.Add(nameTextBox);
+            this.Controls.Add(phoneLabel);
             this.Controls.Add(phoneNumberTextBox);
             this.Controls.Add(addContactButton);
             this.Controls.Add(removeContactButton);
+            this.Controls.Add(searchLabel);
             this.Controls.Add(searchTextBox);
             this.Controls.Add(searchButton);
             this.Controls.Add(contactsListBox);
         }
+
 
         private void UpdateContactsList()
         {
@@ -197,6 +225,16 @@ namespace ContactManager
                 MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+
+            this.ClientSize = new System.Drawing.Size(347, 294);
+            this.Name = "ContactForm";
+            this.ResumeLayout(false);
+
         }
     }
 }
